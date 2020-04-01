fa8c31eabd83d12d85e1130ac6fae3753642863d
MariaLPF <liriopf@gmail.com>
2020-04-01 09:33:39 +0200

    Diseño basico parte 3

--- lib/src/pages/basic_page.dart
+++ lib/src/pages/basic_page.dart
@@ -11,45 +11,56 @@ class BasicPage extends StatelessWidget {
 
     return Scaffold(
       body: Center(
-        child: Column(
-          
-          children:<Widget> [
-            _createImageHeader(),
-            _createImageDescription(textTileStyle, textSubtitleStyle),
-            _createAcctions(),
-            _crearText()
-            ]
+        child: SingleChildScrollView(
+           child: Column(
+              children:<Widget> [
+              _createImageHeader(),
+              _createImageDescription(textTileStyle, textSubtitleStyle),
+              _createAcctions(),
+              _crearText(),
+              _crearText(),
+              _crearText(),
+              ]
+            ),
           ),
       ),
     );
   }
 
   Widget _createImageHeader() {
-    return Image(image: NetworkImage('https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'));
+    return Container(
+      width: double.infinity,
+      child: Image(
+        image: NetworkImage('https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'),
+        fit: BoxFit.cover,
+      ),
+    );
   }
 
   Widget _createImageDescription(TextStyle textTileStyle, TextStyle textSubtitleStyle){
-    return Container(
-            padding:EdgeInsets.symmetric(horizontal:30.0, vertical: 20.0),
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child:Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text('Gray bridge and trees',style: textTileStyle),
-                      //Padding(padding: EdgeInsets.only(top:7.0)),
-                      SizedBox(height:7.0), //Efecto visual equivale al padding anterior
-                      Text('Gablenz-Alemania', style: textSubtitleStyle)
-                    ],
+    return SafeArea(
+          child: Container(
+              padding:EdgeInsets.symmetric(horizontal:30.0, vertical: 20.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child:Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('Gray bridge and trees',style: textTileStyle),
+                        //Padding(padding: EdgeInsets.only(top:7.0)),
+                        SizedBox(height:7.0), //Efecto visual equivale al padding anterior
+                        Text('Gablenz-Alemania', style: textSubtitleStyle)
+                      ],
+                    ),
                   ),
-                ),
-                Icon(Icons.star, color: Colors.red, size: 30.0,),
-                Text('41', style: TextStyle(fontSize:20.0),),
-                
-              ],
+                  Icon(Icons.star, color: Colors.red, size: 30.0,),
+                  Text('41', style: TextStyle(fontSize:20.0),),
+                  
+                ],
+              ),
             ),
-          );
+    );
   }
 
   Widget _createAcctions() {
@@ -74,12 +85,14 @@ class BasicPage extends StatelessWidget {
   }
 
   Widget _crearText() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal:40.0, vertical:20.0),
-      child: Text(
-        'Eiusmod et sit est adipisicing irure esse pariatur sit. Sit dolor cupidatat aliqua nisi tempor ex eiusmod laborum eu veniam amet nulla fugiat. Ullamco amet esse nulla esse Lorem nisi laboris laboris velit qui nulla id.',
-        textAlign: TextAlign.justify,
-        style: TextStyle(fontSize:18.0),
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal:40.0, vertical:20.0),
+        child: Text(
+          'Eiusmod et sit est adipisicing irure esse pariatur sit. Sit dolor cupidatat aliqua nisi tempor ex eiusmod laborum eu veniam amet nulla fugiat. Ullamco amet esse nulla esse Lorem nisi laboris laboris velit qui nulla id.',
+          textAlign: TextAlign.justify,
+          style: TextStyle(fontSize:18.0),
+        ),
       ),
     );
   }
